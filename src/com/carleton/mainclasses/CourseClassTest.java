04a68e0bcb13b7d3f142e8394bd1f210b52eabe6
aalkaf <aalkaf@aalkaf-K46CM>
2017-09-20 20:12:36 -0400

    Initilization of project

---
+++ src/com/carleton/mainclasses/CourseClassTest.java
@@ -0,0 +1,27 @@
+package com.carleton.mainclasses;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CourseClassTest {
+
+	@Before
+	public void setUp() throws Exception {
+	Course course = new Course (); 
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTitle() {
+		
+		System.out.println("welcome");
+		fail("Not yet implemented");
+	}
+
+}
