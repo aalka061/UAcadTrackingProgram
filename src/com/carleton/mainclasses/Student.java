3ef5659cc55a7540431fb80994035d3558b1acb6
aalkaf <aalkaf@aalkaf-K46CM>
2017-09-21 16:54:22 -0400

    test 2 and 3 for students method

--- src/com/carleton/mainclasses/Student.java
+++ src/com/carleton/mainclasses/Student.java
@@ -1,5 +1,35 @@
 package com.carleton.mainclasses;
 
 public class Student {
+	
+	public static int number =0;
+	private String name;
+	private int age;
+	
+	public Student (){
+		this.number=0;
+		this.name="UnKnown";
+		this.age=17;
+		
+	}
+	public Student (String name, int  age ){
+		number++;
+		this.name=name;
+		this.age=age;
+		
+	}
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	public int getNumber() {
+		// TODO Auto-generated method stub
+		return number;
+	}
+	public int getAge() {
+		// TODO Auto-generated method stub
+		return age;
+	}
+	
 
 }
