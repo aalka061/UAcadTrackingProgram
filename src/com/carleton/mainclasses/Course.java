d8120f12aebc3e8b630ba597587195b3a9cd084c
aalkaf <aalkaf@aalkaf-K46CM>
2017-09-21 13:35:32 -0400

    test the course code (6 digits and do not start with 0)

--- src/com/carleton/mainclasses/Course.java
+++ src/com/carleton/mainclasses/Course.java
@@ -1,5 +1,48 @@
 package com.carleton.mainclasses;
 
 public class Course {
+	
+	private String title;
+	private int code; 
+
+	public Course(String title,int myCode) {
+		this.title=title; 
+		// first 2 digits represents dept code and the the last the corse code
+		if(!checkCode(myCode)){
+			throw new IllegalArgumentException("Code is invalid");
+			
+		}
+		this.code = myCode; 
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public int getCode() {
+		return code;
+	}
+		
+	
+	public String title() {
+		// TODO Auto-generated method stub
+		if (title!=null){
+			return title;
+		}
+		return null;
+	}
+	public int Code() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	private boolean checkCode(int codeOfCourse){
+		int firstDigitOfCode=  Integer.parseInt(Integer.toString(codeOfCourse).substring(0, 1));
+		if (String.valueOf(codeOfCourse).length()== 6 && firstDigitOfCode!=0){
+			return true;
+		}
+		return false; 
+		
+	}
 
 }
--- src/com/carleton/mainclasses/CourseClassTest.java
+++ src/com/carleton/mainclasses/CourseClassTest.java
@@ -7,10 +7,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CourseClassTest {
-	Course course = new Course ("software developement"); 
+	Course courseUnderTest;
 
 	@Before
 	public void setUp() throws Exception {
+		
+		courseUnderTest= new Course ("software developement",100000); 
 	}
 
 	@After
@@ -19,9 +21,28 @@ public class CourseClassTest {
 
 	@Test
 	public void testTitle() {
-		String courseTitle = course.title();
+		String courseTitle = courseUnderTest.title();
 		assertNotNull(courseTitle);
-	
 	}
+	
+
+	@Test
+	public void testCode() {
+		int courseCode = courseUnderTest.getCode();
+		assertNotNull(courseCode);
+		
+	}
+  // test if code start with 0 will throw exception 
+	@Test(expected = IllegalArgumentException.class)
+	public void constructorShouldThrowllegalArgumentException() {
+	    Course newCourse = new Course("SD",011111);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void constructorShouldThrowIllegalArgumentExceptionTest2() {
+	    Course newCourse = new Course("SD",1111);
+	}
+	
+	
 
 }
