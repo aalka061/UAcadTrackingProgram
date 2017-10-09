f4db4790a68e8457746ab600e0fdf6465dc2fd16
aalkaf <aalkaf@aalkaf-K46CM>
2017-10-09 19:35:45 -0400

    cleaning up tests of course class

--- src/server/logic/tests/CourseTests.java
+++ src/server/logic/tests/CourseTests.java
@@ -14,18 +14,16 @@ import server.logic.model.Student;
 
 public class CourseTests {
 	Course courseUnderTest;
-
-	@Before
-	public void setUp() throws Exception {
-		courseUnderTest= new Course ("software developement",100000); 
-	}
-
+//	public Course (String title,boolean enforcePrereqs, int numberOfMidterms, int numberOfAssignments, 
+	//		boolean hasAFinal, int capSize, int code )
+	
 	@After
 	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public void testTitle() {
+		courseUnderTest= new Course("OOD",false,0,1,true,26,110111); 
 		String courseTitle = courseUnderTest.title();
 		assertNotNull(courseTitle);
 	}
@@ -33,6 +31,7 @@ public class CourseTests {
 
 	@Test
 	public void testCode() {
+		courseUnderTest= new Course("OOD",false,0,1,true,26,110111); 
 		int courseCode = courseUnderTest.getCode();
 		assertNotNull(courseCode);
 		
@@ -40,18 +39,19 @@ public class CourseTests {
   // test if code start with 0 will throw exception 
 	@Test(expected = IllegalArgumentException.class)
 	public void constructorShouldThrowllegalArgumentException() {
-	    Course newCourse = new Course("SD",011111);
+		courseUnderTest= new Course("OOD",false,0,1,true,26,011111); 
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void constructorShouldThrowIllegalArgumentExceptionTest2() {
-	    Course newCourse = new Course("SD",1111);
+	public void constructorShouldThrowIllegalArgumentExceptionTest_lessThanAnticpatedDigits() {
+		courseUnderTest= new Course("OOD",false,0,1,true,26,11011); 
 	}
 	
 	
 	@Test
 	public void testStudents() {
 		// test 1 - size of student list after adding a student to the course
+		courseUnderTest= new Course("OOD",false,0,1,true,26,110111); 
 		courseUnderTest.addStudent(new Student());
 		List<Student>studentList= courseUnderTest.Students();
 		List <Student> expcectedList = new ArrayList<Student>();
@@ -80,6 +80,7 @@ public class CourseTests {
 	@Test
 	public void removeStudent_removingStudentFromCourse(){
 		// test 1 - remove a student from the list
+		courseUnderTest= new Course("OOD",false,0,1,true,26,110111); 
 		courseUnderTest.addStudent(new Student());
 		Student newStudent = new Student ();
 		courseUnderTest.addStudent(newStudent);
@@ -98,6 +99,8 @@ public class CourseTests {
 	public void weightOfAssignment_NoAssignement_returingZero(){
 		//assignemnt number is from 0 to 5
 		// test 1 if it the parameter is 0 then the function should return 0
+		courseUnderTest= new Course("OOD",false,0,1,true,26,110111); 
+
 		int expectedWeight = 0;
 		int actualWeight = courseUnderTest.weightOfAssignment(0);
 		assertEquals(expectedWeight,actualWeight);
@@ -106,7 +109,7 @@ public class CourseTests {
 	@Test
 	public void weightOfAssignment_OneAssignementNoPorjectNoFinal(){
 		//assignemnt number is from 0 to 5
-		Course tempCourse = new Course("Object Oriented",true,0,1,false,27);
+		Course tempCourse = new Course("Object Oriented",true,0,1,false,27,110111);
 		// test 1 if it the parameter is 0 then the function should return 0
 		int expectedWeight = 100;
 		int actualWeight = tempCourse.weightOfAssignment(1);
@@ -116,8 +119,10 @@ public class CourseTests {
 	public void weightOfMidterm_NoMidterm_returingZero(){
 		//midtermNum number is from 0 to 2
 		//if the parameter is 0 then the function should return 0
+		courseUnderTest= new Course("OOD",false,0,1,true,26,110111); 
+
 		int expectedWeight = 0;
-		int actualWeight = courseUnderTest.WeightOfMidterm(0);
+		int actualWeight = courseUnderTest.weightOfMidterm(0);
 		assertEquals(expectedWeight,actualWeight);
 	}
 	
@@ -144,18 +149,22 @@ public class CourseTests {
 	public void weightOfFinal_hasFinalIsFalse_returingZero(){
 		//midtermNum number is from 0 to 2
 		//if the parameter is 0 then the function should return 0
+		courseUnderTest= new Course("OOD",false,0,1,false,26,110111); 
+
 		int expectedWeight = 0;
-		int actualWeight = courseUnderTest.WeightOfFinal();
+		int actualWeight = courseUnderTest.weightOfFinal();
 		assertEquals(expectedWeight,actualWeight);
 	}
 	
 	@Test
-	public void weightOfFinal_hasFinalIsTrue_returingFiftyForNow(){
+	public void weightOfFinal_hasFinalIsTrue_returningFiftyForNow(){
 		//midtermNum number is from 0 to 2
 		//if the parameter is 0 then the function should return 0
+		courseUnderTest= new Course("OOD",false,0,1,true,26,110111); 
+
 		int expectedWeight = 50;
 		courseUnderTest.setHasAFinal(true);
-		int actualWeight = courseUnderTest.WeightOfFinal();
+		int actualWeight = courseUnderTest.weightOfFinal();
 		assertEquals(expectedWeight,actualWeight);
 	}
 	
@@ -163,11 +172,11 @@ public class CourseTests {
 	public void isFull_isNumberOfStudentsAtTheCourseReachedTheLimt_returningTrue(){
 		//midtermNum number is from 0 to 2
 		//if the parameter is 0 then the function should return 0
-		Course newCourseUnderTest = new Course("OOD",false,0,1,true,27);
-		for (int i=0; i<newCourseUnderTest.getCapSize();i++){
-			newCourseUnderTest.addStudent(new Student("Ahmed",i+5, false));
+		courseUnderTest= new Course("OOD",false,0,1,true,26,110111); 
+		for (int i=0; i<courseUnderTest.getCapSize();i++){
+			courseUnderTest.addStudent(new Student("Ahmed",i+5, false));
 		}
-		assertTrue(newCourseUnderTest.isFull());
+		assertTrue(courseUnderTest.isFull());
 	}
 	
 	
