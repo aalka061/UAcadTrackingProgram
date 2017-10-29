16e78289847c28d81715cdac9d150d6789bd3bde
aalkaf <aalkaf@aalkaf-K46CM>
2017-10-29 15:49:20 -0400

    clerk cancels a course successfully

--- src/server/logic/tests/AcceptanceTests.java
+++ src/server/logic/tests/AcceptanceTests.java
@@ -6,18 +6,24 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import server.logic.model.Course;
 import server.logic.tables.CourseTabel;
+import server.logic.tables.StudentTabel;
 import server.logic.tables.UniversityTable;
 
 public class AcceptanceTests {
 	UniversityTable universityTable= null;
 	CourseTabel courseTable = null;
+	StudentTabel studentTable = null;
+
 	
 	@Before
 	public void setUp() throws Exception {
 		
 		universityTable = UniversityTable.getInstance();
 		courseTable = CourseTabel.getInstance();
+		studentTable = StudentTabel.getInstance();
+		
 	//	universityTable.clearUniversityTable();
 	}
 
@@ -46,6 +52,49 @@ public class AcceptanceTests {
 		int expectedNumberOfCourses = 5;  
 		int actualNumberOfCourses = courseTable.size();
 		assertEquals(expectedNumberOfCourses, actualNumberOfCourses);
+		
+	}
+	
+	@Test
+	public void clerkCreatesStudent_success() {
+		
+		universityTable.createstudent("Simon", 17, false);
+		// 4/5 is automatically generated +1 for the new one
+		int expectedNumberOfStudents = 5;  
+		int actualNumberOfStudents = studentTable.size();
+		assertEquals(expectedNumberOfStudents, actualNumberOfStudents);
+		
+	}
+	
+	@Test
+	public void clerkCreatesStudent_fail_sameStudentName() {
+		
+		universityTable.createstudent("Emley", 17, false);
+		// 4/5 is automatically generated +1 for the new one
+		int expectedNumberOfStudents = 5;  
+		int actualNumberOfStudents = studentTable.size();
+		assertEquals(expectedNumberOfStudents, actualNumberOfStudents);
+		
+	}
+	
+	@Test
+	public void clerkCancelsCOourse_success() {
+		
+		int existedCourseCode = 100001;
+		int existedStudentNum = 1;
+		// register a student to a course 
+		universityTable.registerStudentForCourse(existedStudentNum, existedCourseCode);
+		// find that course 
+		Course course = courseTable.findCourse(existedCourseCode);
+		int numberOfStudents =course.Students().size();
+		int expectedNumberOfStudent= 1;
+		assertEquals(expectedNumberOfStudent, numberOfStudents);
+		// cancel the course which means the course should not have any more students
+		universityTable.cancelCourse(existedCourseCode);
+		numberOfStudents =course.Students().size();
+		expectedNumberOfStudent= 0;
+		
+		assertEquals(expectedNumberOfStudent, numberOfStudents);
 		
 	}
 
